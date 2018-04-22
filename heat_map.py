b29c6e3300f11d9dcbf6679583c1cfb3ae33a40b
Carl Martin Krokeide <carlmk@clab10.idi.ntnu.no>
2018-04-22 19:47:20 +0200

    oooh

--- heat_map.py
+++ heat_map.py
@@ -6,17 +6,21 @@ from keras.preprocessing.image import img_to_array
 from keras.models import load_model
 from keras.layers import Activation
 from vis.utils import utils
+from vis.visualization import visualize_cam
+from vis.visualization import visualize_saliency, overlay
 
 def heat_map():
 
     FRAME_H = 64
     FRAME_W = 64
 
-    model = load_model('trained_model.h5')
-    img = utils.load_img('IMG/image684.jpg')
+    model = load_model('trained_model_simulator.h5')
+    img = utils.load_img('IMG_LEFT/center_2018_04_22_16_24_41_584.jpg')
+    #img = utils.load_img('IMG_LEFT/center_2018_04_22_16_23_27_935.jpg')
+    #img = utils.load_img('IMG_LEFT/center_2018_04_22_16_23_34_066.jpg')
 
     shape = img.shape
-    img = img[math.floor(shape[0]/4):, 0:shape[1]] #removed shape[0]-25 from row
+    img = img[math.floor(shape[0]/4):shape[0]-25, 0:shape[1]]
 
     target_size=(FRAME_H, FRAME_W)
     img = transform.resize(img, target_size, preserve_range=True).astype('uint8')
@@ -26,33 +30,25 @@ def heat_map():
     plt.imshow(img)
     plt.show()
 
-    # Convert to BGR, create input with batch_size: 1.
-    bgr_img = utils.bgr2rgb(img)
-    img_input = np.expand_dims(img_to_array(bgr_img), axis=0)
+    img_input = np.expand_dims(img_to_array(img), axis=0)
+
     pred = model.predict(img_input)[0][0]
     print('Predicted {}'.format(pred))
-
-    for layer in model.layers:
-        layer.trainable = True
-
-    from vis.visualization import visualize_saliency, overlay
 
     titles = ['right steering', 'left steering', 'maintain steering']
     modifiers = [None, 'negate', 'small_values']
 
+
     for i, modifier in enumerate(modifiers):
-        heatmap = visualize_saliency(model, layer_idx=-1, filter_indices=0, seed_input=bgr_img, grad_modifier=modifier)
+        heatmap = visualize_saliency(model, layer_idx=-1, filter_indices=None, seed_input=img, grad_modifier=modifier) #, filter_indices=0
         plt.figure()
         plt.title(titles[i])
         # Overlay is used to alpha blend heatmap onto img.
         plt.imshow(overlay(img, heatmap, alpha=0.7))
         plt.show()
 
-    from vis.visualization import visualize_cam
-
     for i, modifier in enumerate(modifiers):
-        heatmap = visualize_cam(model, layer_idx=-1, filter_indices=0,
-                                seed_input=bgr_img, grad_modifier=modifier)
+        heatmap = visualize_cam(model, layer_idx=-1, filter_indices=None, seed_input=img, grad_modifier=modifier) #, filter_indices=0
         plt.figure()
         plt.title(titles[i])
         # Overlay is used to alpha blend heatmap onto img.
