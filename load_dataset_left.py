6ccd12fe1476513e8d9e226a71fb4e6cc5b0b0fc
Carl Martin Krokeide <carlmk@clab10.idi.ntnu.no>
2018-04-22 16:52:13 +0200

    Dataset from simualtor

---
+++ load_dataset_left.py
@@ -0,0 +1,41 @@
+import glob
+import os
+import pandas as pd
+import numpy as np
+import cv2
+import math
+import matplotlib.pyplot as plt
+
+
+def load_dataset():
+
+    csv_path = 'driving_log_left.csv'
+
+    data_files = pd.read_csv(csv_path, index_col = False)
+    data_files['direction'] = pd.Series('s', index=data_files.index)
+    data_files.columns = ['center','left','right','steering','throttle','break']
+
+    data_size = len(data_files)
+
+    np_images = np.zeros((data_size, 64, 64, 3))
+    np_steering = np.zeros(data_size)
+
+    for i_elem in range(data_size):
+
+        image = cv2.imread(data_files['center'][i_elem].strip())
+
+        if image is not None:
+
+          shape = image.shape
+
+          image = image[math.floor(shape[0]/4):shape[0]-25, 0:shape[1]]
+          image = cv2.resize(image,(64,64), interpolation=cv2.INTER_AREA)
+          image = image/255.-.5
+          image = np.array(image)
+
+          steer = np.array([data_files['steer'][i_elem]])
+
+          np_images[i_elem] = image
+          np_steering[i_elem] = steer
+
+    return np_images, np_steering;
--- main.py
+++ main.py
@@ -1,30 +1,40 @@
 import vgg16
-import load_dataset
+import load_dataset_left
+import load_dataset_right
+import load_dataset_spurv
 import save_load_model
 import heat_map
 from keras.models import load_model
 
 def main():
 
-    print("Creating model...")
+    #print("Creating model...")
 
-    vgg16.vgg16()
+    #vgg16.vgg16()
 
     print("Loading model...")
 
-    model = load_model("vgg16_test.h5")
+    #model = load_model("vgg16_test.h5")
 
-    print("Loading dataset...")
+    print("Loading dataset left...")
 
-    np_images, np_steering = load_dataset.load_dataset()
+    np_images, np_steering = load_dataset_left.load_dataset()
 
     print("Training the model...")
 
-    history = model.fit(np_images, np_steering, epochs=5, batch_size=10)
+    history = model.fit(np_images, np_steering, epochs=5, batch_size=32)
+
+    print("Loading dataset right...")
+
+    np_images, np_steering = load_dataset_right.load_dataset()
+
+    print("Training the model...")
+
+    history = model.fit(np_images, np_steering, epochs=5, batch_size=32)
 
     print("Saving the model...")
 
-    save_load_model.save_model(model, "trained_model")
+    save_load_model.save_model(model, "trained_model_simulator")
 
     #print("Creating heatmap...")
 
