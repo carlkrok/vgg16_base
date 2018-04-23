86dc21e44370f885de3d99f536c0b372f2ffda9b
Carl Martin Krokeide <carlmk@clab13.idi.ntnu.no>
2018-04-23 19:23:36 +0200

    Added datasets

--- main.py
+++ main.py
@@ -22,8 +22,9 @@ def main():
 
     for dataset in ["LEFT", "RIGHT", "mond1", "mond2", "mond3", "mond4"]:
         for camera_angle in ["center", "right", "left"]:
+            print("Currently training on dataset: ", dataset, ", angle: ", camera_angle, ".")
             np_images, np_steering = load_dataset_simulator.load_dataset(camera_angle,dataset)
-            history = model.fit(x=np_images, y=np_steering, epochs=5, batch_size=5, callbacks=[checkpoint], validation_data=(np_val_images, np_val_steering))
+            history = model.fit(x=np_images, y=np_steering, epochs=10, batch_size=5, callbacks=[checkpoint], validation_data=(np_val_images, np_val_steering))
 '''
     print("Loading dataset LEFT - center...")
     np_images, np_steering = load_dataset_simulator.load_dataset("center","LEFT")
