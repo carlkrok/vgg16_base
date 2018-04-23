d53d834d225d65c9f4aab082a970d5c587e02445
Carl Martin Krokeide <carlmk@clab13.idi.ntnu.no>
2018-04-23 20:38:53 +0200

    Added datasets

--- main.py
+++ main.py
@@ -13,8 +13,9 @@ def main():
     vgg16.vgg16()
 
     print("Loading model...")
-    model = load_model("vgg16_test.h5")
+    #model = load_model("vgg16_test.h5")
     #model = load_model("trained_model_simulator.h5")
+    model = load_model("model-010.h5")
 
     checkpoint = ModelCheckpoint('model-{epoch:03d}.h5', monitor='val_loss',verbose=0,save_best_only=True, mode='auto') #Saved_models/
 
@@ -25,8 +26,8 @@ def main():
             print("Currently training on dataset: ", dataset, ", angle: ", camera_angle, ".")
             np_images, np_steering = load_dataset_simulator.load_dataset(camera_angle,dataset)
             history = model.fit(x=np_images, y=np_steering, epochs=10, batch_size=5, callbacks=[checkpoint], validation_data=(np_val_images, np_val_steering))
-            print("Saving the model...")
-            save_load_model.save_model(model, "trained_model_simulator")
+            #print("Saving the model...")
+            #save_load_model.save_model(model, "trained_model_simulator")
 
     print("Saving the model...")
     save_load_model.save_model(model, "trained_model_simulator")
